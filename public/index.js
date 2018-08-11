bf043a2ba881250d8632162cd258fcc7537459f6
kslohith <kslohith1729@gmail.com>
2018-08-12 00:15:21 +0530

    node http

--- public/index.js
+++ public/index.js
@@ -1,5 +1,3 @@
-
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -7,41 +5,41 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer( (req , res) => {
-      console.log(req.headers);
+	console.log(req.headers);
 
-      if( req.method == 'GET' )
-      {
-      var fileUrl;
-      if(req.url == '/') fileUrl = '/index.html';
-      else fileUrl = req.url;
+	if( req.method == 'GET' )
+	{
+	var fileUrl;
+	if(req.url == '/') fileUrl = '/index.html';
+	else fileUrl = req.url;
 
-      var filePath = path.resolve('./public' + fileUrl);
-      const fileExt = path.extname(filePath);
-      console.log(filePath);
-      if( fileExt == '.html' ) {
-            fs.exists(filePath , (exists) => {
+	var filePath = path.resolve(fileUrl);
+	const fileExt = path.extname(filePath);
+	console.log(filePath);
+	if( fileExt == '.html' ) {
+		fs.exists(filePath , (exists) => {
           if(!exists) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type' , 'text/html');
-            res.end('<html><body><h1>Error 404: Not found(not exist)</h1></body></html>');
-            return;
+          	res.statusCode = 404;
+          	res.setHeader('Content-Type' , 'text/html');
+          	res.end('<html><body><h1>Error 404: Not found(not exist)</h1></body></html>');
+          	return;
           }
           res.statusCode = 200;
           res.setHeader('Content-Type' , 'text/html');
           fs.createReadStream(filePath).pipe(res);
-            });
-      }
-      else
-      {
-            res.statusCode = 404;
+		});
+	}
+	else
+	{
+		res.statusCode = 404;
         res.setHeader('Content-Type' , 'text/html');
         res.end('<html><body><h1>Error :Not found(not html)</h1></body></html>');
         return;
-      }
+	}
    }
    else
    {
-            res.statusCode = 404;
+   		res.statusCode = 404;
         res.setHeader('Content-Type' , 'text/html');
         res.end('<html><body><h1>Error :Access method not supported</h1></body></html>');
         return;
@@ -52,4 +50,4 @@ const server = http.createServer( (req , res) => {
 server.listen(port , hostname , () => {
        console.log(`server running at http:${hostname}:${port}/`);
 });
-       
+	 
